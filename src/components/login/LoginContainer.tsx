2ed44ec0f6daa0666c0989034d14c197c872134a
aluna <algimenez@fi.uba.ar>
2025-10-19 01:26:18 +0000

    update

--- src/components/login/LoginContainer.tsx
+++ src/components/login/LoginContainer.tsx
@@ -10,7 +10,7 @@ export const LoginContainer = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center border-2 border-blue-400 px-4 py-8">
             <LoginHeader />
-            <h2 className="text-blue-400 font-pixel text-sm md:text-lg text-center">ACCESO AL REINO<br />Ingresa tus credenciales de aventurero</h2>
+            <h2 className="text-sm">ACCESO AL REINO<br />Ingresa tus credenciales de aventurero</h2>
             <LoginForm onLoginSuccess={handleLoginSuccess} />
         </div>
     )
